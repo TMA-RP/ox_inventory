0c4e0d3a3d51a987a4c617ab24e45e3ef4f05f2a
Cyril Beeckman <cyril.beeckman@gmail.com>
2022-12-10 18:30:15 +0100

    our modif

--- client.lua
+++ client.lua
@@ -267,7 +267,8 @@ local function canUseItem(isAmmo)
 	and not PlayerData.dead
 	and not invBusy
 	and not lib.progressActive()
-	and (IsPlayerFreeForAmbientTask(cache.playerId) or cache.vehicle or (isAmmo and currentWeapon and IsPlayerFreeAiming(cache.playerId)))
+    and not IsPedRagdoll(cache.ped) and not IsPedFalling(cache.ped)
+	and (not isAmmo or (isAmmo and currentWeapon and IsPlayerFreeAiming(cache.playerId)))
 end
 
 ---@param data table
