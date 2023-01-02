77e4236035fe8e1c18187183870d77d6bb9cece7
Vincent <92932421+THEM3RY@users.noreply.github.com>
2023-01-03 00:20:22 +0100

    our modif

--- data/stashes.lua
+++ data/stashes.lua
@@ -13,40 +13,92 @@
 
 return {
 	{
-		coords = vec3(452.3, -991.4, 30.7),
-		target = {
-			loc = vec3(451.25, -994.28, 30.69),
-			length = 1.2,
-			width = 5.6,
-			heading = 0,
-			minZ = 29.49,
-			maxZ = 32.09,
-			label = 'Open personal locker'
-		},
+		coords = vec3(359.3871, -1599.8046, 29.3072),
 		name = 'policelocker',
-		label = 'Personal locker',
-		owner = true,
-		slots = 70,
-		weight = 70000,
-		groups = shared.police
+		label = 'Coffre équipements',
+		owner = false,
+		slots = 100,
+		weight = 100000,
+		groups = {['police'] = 1}
 	},
 
 	{
-		coords = vec3(301.3, -600.23, 43.28),
-		target = {
-			loc = vec3(301.82, -600.99, 43.29),
-			length = 0.6,
-			width = 1.8,
-			heading = 340,
-			minZ = 43.34,
-			maxZ = 44.74,
-			label = 'Open personal locker'
-		},
-		name = 'emslocker',
-		label = 'Personal Locker',
-		owner = true,
+		coords = vec3(1837.6097, 3684.6589, 34.1893),
+		name = 'sandysherifflocker',
+		label = 'Coffre équipements',
+		owner = false,
+		slots = 100,
+		weight = 100000,
+		groups = {['sheriff'] = 1}
+	},
+
+	{
+		coords = vec3(-449.0974, 6015.1670, 36.9957),
+		name = 'paletosherifflocker',
+		label = 'Coffre équipements',
+		owner = false,
+		slots = 100,
+		weight = 100000,
+		groups = {['sheriff'] = 1}
+	},
+
+	{
+		coords = vec3(345.4393, -578.3614, 43.2654),
+		name = 'lsemslocker',
+		label = 'Rangement médical',
+		owner = false,
 		slots = 70,
 		weight = 70000,
 		groups = {['ambulance'] = 0}
+	},
+
+	{
+		coords = vec3(1660.8080, 3660.0210, 35.3397),
+		name = 'sandyemslocker',
+		label = 'Rangement médical',
+		owner = false,
+		slots = 70,
+		weight = 70000,
+		groups = {['ambulance'] = 0}
+	},
+
+	{
+		coords = vec3(-432.4435, 6145.5688, 32.3130),
+		name = 'paletomayorlocker',
+		label = 'Rangement de Monsieur le Maire',
+		owner = false,
+		slots = 70,
+		weight = 70000,
+		groups = {['gouv'] = 9}
+	},
+
+	{
+		coords = vec3(-535.5236, -195.2401, 43.3659),
+		name = 'lsmayorlocker',
+		label = 'Rangement de Monsieur le Maire',
+		owner = false,
+		slots = 70,
+		weight = 70000,
+		groups = {['gouv'] = 9}
+	},
+
+	{
+		coords = vec3(-529.8237, -186.9237, 43.3659),
+		name = 'gouvlocker',
+		label = 'Rangement de Monsieur le gouverneur',
+		owner = false,
+		slots = 70,
+		weight = 70000,
+		groups = {['gouv'] = 11}
+	},
+
+	{
+		coords = vec3(-586.8375, -203.6964, 38.2271),
+		name = 'judgelocker',
+		label = 'Rangement de Monsieur le juge',
+		owner = false,
+		slots = 70,
+		weight = 70000,
+		groups = {['lawyer'] = 2}
 	},
 }
