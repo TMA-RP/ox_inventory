88721b3cc6c153d49cad3aaa22dae158140851ab
Cyril Beeckman <cyril.beeckman@gmail.com>
2023-12-08 04:04:52 +0100

    our modif

--- modules/items/client.lua
+++ modules/items/client.lua
@@ -4,39 +4,38 @@ local Items = require 'modules.items.shared' --[[@as table<string, OxClientItem>
 
 local function sendDisplayMetadata(data)
     SendNUIMessage({
-		action = 'displayMetadata',
-		data = data
-	})
+        action = 'displayMetadata',
+        data = data
+    })
 end
 
 --- use array of single key value pairs to dictate order
 ---@param metadata string | table<string, string> | table<string, string>[]
 ---@param value? string
 local function displayMetadata(metadata, value)
-	local data = {}
+    local data = {}
 
-	if type(metadata) == 'string' then
+    if type(metadata) == 'string' then
         if not value then return end
 
         data = { { metadata = metadata, value = value } }
-	elseif table.type(metadata) == 'array' then
-		for i = 1, #metadata do
-			for k, v in pairs(metadata[i]) do
-				data[i] = {
-					metadata = k,
-					value = v,
-				}
-			end
-		end
-	else
-		for k, v in pairs(metadata) do
-			data[#data + 1] = {
-			data[#data + 1] = {
-				metadata = k,
-				value = v,
-			}
-		end
-	end
+    elseif table.type(metadata) == 'array' then
+        for i = 1, #metadata do
+            for k, v in pairs(metadata[i]) do
+                data[i] = {
+                    metadata = k,
+                    value = v,
+                }
+            end
+        end
+    else
+        for k, v in pairs(metadata) do
+            data[#data + 1] = {
+                metadata = k,
+                value = v,
+            }
+        end
+    end
 
     if client.uiLoaded then
         return sendDisplayMetadata(data)
@@ -55,33 +54,33 @@ exports('displayMetadata', displayMetadata)
 ---@param name string?
 ---@return table?
 local function getItem(_, name)
-	if not name then return Items end
+    if not name then return Items end
 
-	if type(name) ~= 'string' then return end
+    if type(name) ~= 'string' then return end
 
-	name = name:lower()
+    name = name:lower()
 
-	if name:sub(0, 7) == 'weapon_' then
-		name = name:upper()
-	end
+    if name:sub(0, 7) == 'weapon_' then
+        name = name:upper()
+    end
 
-	return Items[name]
+    return Items[name]
 end
 
 setmetatable(Items --[[@as table]], {
-	__call = getItem
+    __call = getItem
 })
 
 ---@cast Items +fun(itemName: string): OxClientItem
 ---@cast Items +fun(): table<string, OxClientItem>
 
 local function Item(name, cb)
-	local item = Items[name]
-	if item then
-		if not item.client?.export and not item.client?.event then
-			item.effect = cb
-		end
-	end
+    local item = Items[name]
+    if item then
+        if not item.client?.export and not item.client?.event then
+            item.effect = cb
+        end
+    end
 end
 
 local ox_inventory = exports[shared.resource]
@@ -113,21 +112,21 @@ local ox_inventory = exports[shared.resource]
 
 client.parachute = false
 Item('parachute', function(data, slot)
-	if not client.parachute then
-		ox_inventory:useItem(data, function(data)
-			if data then
-				local chute = `GADGET_PARACHUTE`
-				SetPlayerParachuteTintIndex(PlayerData.id, 6)
-				GiveWeaponToPed(cache.ped, chute, 0, true, false)
-				SetPedGadget(cache.ped, chute, true)
-				lib.requestModel(1269906701)
-				client.parachute = CreateParachuteBagObject(cache.ped, true, true)
-				if slot.metadata.type then
-					SetPlayerParachuteTintIndex(PlayerData.id, slot.metadata.type)
-				end
-			end
-		end)
-	end
+    if not client.parachute then
+        ox_inventory:useItem(data, function(data)
+            if data then
+                local chute = `GADGET_PARACHUTE`
+                SetPlayerParachuteTintIndex(PlayerData.id, 6)
+                GiveWeaponToPed(cache.ped, chute, 0, true, false)
+                SetPedGadget(cache.ped, chute, true)
+                lib.requestModel(1269906701)
+                client.parachute = CreateParachuteBagObject(cache.ped, true, true)
+                if slot.metadata.type then
+                    SetPlayerParachuteTintIndex(PlayerData.id, slot.metadata.type)
+                end
+            end
+        end)
+    end
 end)
 
 -- Item('phone', function(data, slot)
@@ -141,50 +140,50 @@ end)
 -- end)
 
 Item('clothing', function(data, slot)
-	local metadata = slot.metadata
+    local metadata = slot.metadata
 
-	if not metadata.drawable then return print('Clothing is missing drawable in metadata') end
-	if not metadata.texture then return print('Clothing is missing texture in metadata') end
+    if not metadata.drawable then return print('Clothing is missing drawable in metadata') end
+    if not metadata.texture then return print('Clothing is missing texture in metadata') end
 
-	if metadata.prop then
-		if not SetPedPreloadPropData(cache.ped, metadata.prop, metadata.drawable, metadata.texture) then
-			return print('Clothing has invalid prop for this ped')
-		end
-	elseif metadata.component then
-		if not IsPedComponentVariationValid(cache.ped, metadata.component, metadata.drawable, metadata.texture) then
-			return print('Clothing has invalid component for this ped')
-		end
-	else
-		return print('Clothing is missing prop/component id in metadata')
-	end
+    if metadata.prop then
+        if not SetPedPreloadPropData(cache.ped, metadata.prop, metadata.drawable, metadata.texture) then
+            return print('Clothing has invalid prop for this ped')
+        end
+    elseif metadata.component then
+        if not IsPedComponentVariationValid(cache.ped, metadata.component, metadata.drawable, metadata.texture) then
+            return print('Clothing has invalid component for this ped')
+        end
+    else
+        return print('Clothing is missing prop/component id in metadata')
+    end
 
-	ox_inventory:useItem(data, function(data)
-		if data then
-			metadata = data.metadata
+    ox_inventory:useItem(data, function(data)
+        if data then
+            metadata = data.metadata
 
-			if metadata.prop then
-				local prop = GetPedPropIndex(cache.ped, metadata.prop)
-				local texture = GetPedPropTextureIndex(cache.ped, metadata.prop)
+            if metadata.prop then
+                local prop = GetPedPropIndex(cache.ped, metadata.prop)
+                local texture = GetPedPropTextureIndex(cache.ped, metadata.prop)
 
-				if metadata.drawable == prop and metadata.texture == texture then
-					return ClearPedProp(cache.ped, metadata.prop)
-				end
+                if metadata.drawable == prop and metadata.texture == texture then
+                    return ClearPedProp(cache.ped, metadata.prop)
+                end
 
-				-- { prop = 0, drawable = 2, texture = 1 } = grey beanie
-				SetPedPropIndex(cache.ped, metadata.prop, metadata.drawable, metadata.texture, false);
-			elseif metadata.component then
-				local drawable = GetPedDrawableVariation(cache.ped, metadata.component)
-				local texture = GetPedTextureVariation(cache.ped, metadata.component)
+                -- { prop = 0, drawable = 2, texture = 1 } = grey beanie
+                SetPedPropIndex(cache.ped, metadata.prop, metadata.drawable, metadata.texture, false);
+            elseif metadata.component then
+                local drawable = GetPedDrawableVariation(cache.ped, metadata.component)
+                local texture = GetPedTextureVariation(cache.ped, metadata.component)
 
-				if metadata.drawable == drawable and metadata.texture == texture then
-					return -- item matches (setup defaults so we can strip?)
-				end
+                if metadata.drawable == drawable and metadata.texture == texture then
+                    return -- item matches (setup defaults so we can strip?)
+                end
 
-				-- { component = 4, drawable = 4, texture = 1 } = jeans w/ belt
-				SetPedComponentVariation(cache.ped, metadata.component, metadata.drawable, metadata.texture, 0);
-			end
-		end
-	end)
+                -- { component = 4, drawable = 4, texture = 1 } = jeans w/ belt
+                SetPedComponentVariation(cache.ped, metadata.component, metadata.drawable, metadata.texture, 0);
+            end
+        end
+    end)
 end)
 
 -----------------------------------------------------------------------------------------------
